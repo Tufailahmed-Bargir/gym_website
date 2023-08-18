cd506d4153812736b60e69093a699e1f57b12a6e
Tufailahmed <bargirtufailahmed@gmail.com>
2023-08-18 20:35:41 +0530

    first commit

---
+++ calscript.js
@@ -0,0 +1,15 @@
+
+      // here math.random() function generates the random no between 0 to 1 , 1 is not included in that
+
+      // also math.floor() function is used to round up the number if 12.32 it makes it to only 12 nearest integer
+
+      // *201)+400; to make number range between 400 to 600 this logic is uplicable
+
+    function checkNameField(){
+      const randoNo = Math.floor(Math.random()*201)+400;
+      alert(`your calories is ${randoNo} caloric/mass`);
+      // alert(`your calories is ${randoNo} caloric/mass`);
+    
+    }
+        
+    
